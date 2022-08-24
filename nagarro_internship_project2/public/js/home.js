50b0e6f93735b17e04d46177d9288e276a4b604d
MSI\ABHAY KUMAR <abhay80413@gmail.com>
2022-08-24 16:44:31 +0530

    ecommerce app done

---
+++ nagarro_internship_project2/public/js/home.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+    console.log("home js");
+
+    async function getProducts(){
+        const products = await axios.get('/products',)
+        // console.log(products);
+
+        $('.product-container').empty();
+
+        for(product of products.data){
+            const large = '<div class="card"><div class="title">'+ product.productName +' &nbsp &nbsp <i class="fas fa-rupee-sign"></i>&nbsp ' + product.price+'</div><div class="image"><img src="'+ product.img+'"/></div><div class="text">'+ product.desc+'.</div><button  class="buy-button details"><a href="/product/'+product._id+'">Buy Now</a></button></div>';
+            $('.product-container').prepend(large);
+        }
+    }
+
+    getProducts();
+
+
+   
+
+
+});
+
